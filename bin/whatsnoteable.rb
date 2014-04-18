40c5b5b4ffc4a0efc1ad1656470e5b98135530aa
Phil Wittig <pdwittig@gmail.com>
2014-04-18 01:22:03 -0700

    Added open functionality, but its currently shit

--- bin/whatsnoteable.rb
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env ruby
-
-require_relative '../lib/noteable'
-
-NoteAble.find_all_and_print
--- lib/noteable.rb
+++ lib/noteable.rb
@@ -4,7 +4,11 @@ require_relative 'view'
 
 module NoteAble
 	def self.find_all_and_print
-		View.render_page { return_notes parse_files }
+		View.render_page { render_notes parse_files }
+	end
+
+	def self.open id
+		exec "subl #{parse_files.find { |note| note.id == id.to_i }.filepath}"
 	end
 
 	private
@@ -14,11 +18,12 @@ module NoteAble
 				notes << (Parser.parse file).map! { |note| Note.new(note) }
 			end
 		end
-		notes.reject! { |file_notes| file_notes.length == 0 }
-		notes.flatten.group_by { |note| note.filepath }
+		notes.reject! { |file_notes| file_notes.length == 0 }.flatten!
+		notes
 	end
 
-	def self.return_notes notes
+	def self.render_notes notes
+		notes = notes.flatten.group_by { |note| note.filepath }
 		notes.each do |filepath, notes|
 			View.render_file filepath
 			notes.each { |note| View.render_note note.to_s }
@@ -29,7 +34,7 @@ module NoteAble
 		files.flatten.map { |data| Note.new data }
 	end
 
-	def self.get_files directory
-		directory.each { |file| }
-	end
+	# def self.get_files directory
+	# 	directory.each { |file| }
+	# end
 end
