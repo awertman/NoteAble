78888ce9f551b9990745069cdf0de28e44ffc8e8
Phil Wittig <pdwittig@gmail.com>
2014-04-17 21:03:26 -0700

    Added func to build notes for hash

--- lib/notable.rb
+++ lib/notable.rb
@@ -1,3 +1,10 @@
+require_relative 'note'
+require_relative 'parser'
+require_relative 'view'
+
+include parser
+include view
+
 module Notable
 	def self.find_all_and_print
 		View.render_page { render_notes get_notes }
@@ -5,7 +12,7 @@ module Notable
 
 	private
 	def self.get_notes
-		notes = Parser.Parser.group_by { |note| note.filepath }
+		notes = build_notes(Parser.parse).group_by { |note| note.filepath }
 	end
 
 	def self.render_notes notes
@@ -13,5 +20,9 @@ module Notable
 			View.render_file filepath
 			notes.each { |note| View.render_note note.to_s }
 		end
+	end
+
+	def self.build_notes data_from_files
+		data.flatten.map { |data_from_files| Note.new data_from_files }
 	end
 end
